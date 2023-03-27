cb4af9ccf505557f7c961d317458de351debe965
myairin <irenewindagultom@gmail.com>
2023-03-27 14:10:12 +0700

    update

--- Downloads/skema-2/index.js
+++ Downloads/skema-2/index.js
@@ -1,33 +1,123 @@
-// Mendefinisikan variabel untuk harga kamar per malam
-const data = [
-    { sewa: "1/2 Hari", superior: "100000/Malam", deluxe: "150000/Malam", premium: "200000/Malam" },
-    { sewa: "3/4 Hari", superior: "90000/Malam", deluxe: "135000/Malam", premium: "180000/Malam" },
-    { sewa: ">=5 Hari", superior: "80000/Malam", deluxe: "120000/Malam", premium: "160000/Malam" },
-  ];
-  
-  console.table(data);
-  // console.table(data, { index: false });
-  
-  const readline = require("readline");
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  
-  rl.question("Tipe kamar: \n1. superior\n2. deluxe\n3. premium\nPilih tipe kamar: ", (jenisKamar) => {
-    rl.question("Masukkan lama menginap (dalam hari): ", (lamaMenginap) => {
-      let hargaPerMalam = 0;
-      if (jenisKamar === "1") {
-        hargaPerMalam = lamaMenginap <= 2 ? 100000 : lamaMenginap <= 4 ? 90000 : 80000;
-      } else if (jenisKamar === "2") {
-        hargaPerMalam = lamaMenginap <= 2 ? 150000 : lamaMenginap <= 4 ? 135000 : 120000;
-      } else if (jenisKamar === "3") {
-        hargaPerMalam = lamaMenginap <= 2 ? 200000 : lamaMenginap <= 4 ? 180000 : 160000;
+// import readline
+const readline = require("readline");
+
+// buatlah daftar harga
+const menu = [
+  {
+    type: "Superior",
+    "1-2 hari": 100000,
+    "3-4 hari": 90000,
+    ">=5 hari": 80000,
+  },
+  {
+    type: "Deluxe",
+    "1-2 hari": 150000,
+    "3-4 hari": 135000,
+    ">=5 hari": 120000,
+  },
+  {
+    type: "Premium",
+    "1-2 hari": 200000,
+    "3-4 hari": 180000,
+    ">=5 hari": 160000,
+  },
+];
+
+console.table(menu, ["type", "1-2 hari", "3-4 hari", ">=5 hari"], {
+  showIndex: false,
+});
+console.log("Keterangan :");
+console.log("1. Superior");
+console.log("2. Deluxe");
+console.log("3. Premium");
+
+// tentukan harga
+const hargaHotel = {
+  1: [
+    { hari: 1, harga: 100000 },
+    { hari: 2, harga: 100000 },
+    { hari: 3, harga: 90000 },
+    { hari: 4, harga: 90000 },
+    { hari: 5, harga: 80000 },
+    { hari: Infinity, harga: 80000 },
+  ],
+  2: [
+    { hari: 1, harga: 150000 },
+    { hari: 2, harga: 150000 },
+    { hari: 3, harga: 135000 },
+    { hari: 4, harga: 135000 },
+    { hari: 5, harga: 120000 },
+    { hari: Infinity, harga: 120000 },
+  ],
+  3: [
+    { hari: 1, harga: 200000 },
+    { hari: 2, harga: 200000 },
+    { hari: 3, harga: 180000 },
+    { hari: 4, harga: 180000 },
+    { hari: 5, harga: 160000 },
+    { hari: Infinity, harga: 160000 },
+  ],
+};
+
+// convert menjadi nama
+const tipeKamarMapping = {
+  1: "Superior",
+  2: "Deluxe",
+  3: "Premium",
+};
+
+// process input
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function starts() {
+  rl.question(
+    "Masukkan tipe kamar (input dalam bentuk angka) : ",
+    (tipeKamar) => {
+      if (tipeKamar in tipeKamarMapping) {
+        rl.question(
+          "Masukkan lama menginap (dalam hari) : ",
+          (lamaMenginap) => {
+            const convert = tipeKamarMapping[tipeKamar];
+            const hargaTipeKamar = hargaHotel[tipeKamar];
+            const hargaMalam = hargaTipeKamar.find(
+              (data) => lamaMenginap <= data.hari
+            );
+            const totalHarga = hargaMalam.harga * lamaMenginap;
+
+            console.log(`Tipe Kamar: ${convert}`);
+            console.log(`Lama Menginap: ${lamaMenginap} hari`);
+            console.log(`Total Harga: Rp ${totalHarga}`);
+
+            // askAgain();
+            rl.question("Apakah ada mau pesan lagi ? (y/n)", (answer) => {
+              if (answer === "y") {
+                starts();
+              } else {
+                console.log("terima kasih");
+                rl.close();
+              }
+            });
+          }
+        );
+      } else {
+        console.log("pilihan kamar tidak tersedia");
+        starts();
       }
-  
-      const totalHarga = hargaPerMalam * lamaMenginap;
-  
-      console.log(`Harga kamar ${jenisKamar} selama ${lamaMenginap} hari adalah Rp.${totalHarga}`);
-      rl.close();
-    });
-  });
+    }
+  );
+}
+
+// function askAgain() {
+//   rl.question("Apakah ada mau pesan lagi ? (y/n)", (answer) => {
+//     if (answer === "y") {
+//       starts();
+//     } else {
+//       console.log("terima kasih");
+//       rl.close();
+//     }
+//   });
+// }
+starts();
